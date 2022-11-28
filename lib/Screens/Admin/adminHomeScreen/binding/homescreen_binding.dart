c855a29be4d6cef99e68f874d320e9a761bca1af
Arbash Hussain <arbashhussain08@gmail.com>
2022-11-28 20:12:25 +0530

    admin screen directory

--- lib/Screens/Admin/adminHomeScreen/binding/homescreen_binding.dart
+++
@@ -1,9 +0,0 @@
-import '../controller/homescreen_controller.dart';
-import 'package:get/get.dart';
-
-class adminHomescreenBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => adminHomescreenController());
-  }
-}
